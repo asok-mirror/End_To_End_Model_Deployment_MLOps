cdc0fa5629bab8c948818953c61ce72b8f647087
asokkalidass kalisamy <asok.kalidass@gmail.com>
2021-08-02 17:04:20 -0700

    Added Train and Optimize logic

--- core/data.py
+++ core/data.py
@@ -11,16 +11,18 @@ import pandas as pd
 from feast import FeatureStore
 from datetime import datetime
 from pathlib import Path
+import os
 
-feature_refs=[
-            "credit_card_transactions:V1",
-            "credit_card_transactions:V2",
-            "credit_card_transactions:V3",
-            "credit_card_transactions:V4",
-            "credit_card_transactions:V5",
-            "credit_card_transactions:Time",
-            "credit_card_transactions:Amount",
-        ],
+feature_refs = [
+    "credit_card_transactions:V1",
+    "credit_card_transactions:V2",
+    "credit_card_transactions:V3",
+    "credit_card_transactions:V4",
+    "credit_card_transactions:V5",
+    "credit_card_transactions:Time",
+    "credit_card_transactions:Amount",
+],
+
 
 def download_data():
     """Download the data from URL and save to local drive
@@ -43,19 +45,45 @@ def download_data():
 
 
 def get_historic_features(feature_entity: DataFrame) -> DataFrame:
+    """Gets historic data from feature source
+
+    Returns:
+        DataFrame : historic feature data
+    """
     store = FeatureStore(repo_path=Path(config.BASE_DIR, "features"))
     training_df = store.get_historical_features(
         entity_df=feature_entity,
-        feature_refs = feature_refs
+        feature_refs=feature_refs[0]
     ).to_df()
     return training_df
 
 
 def get_online_features(feature_rows: List[Dict[str, any]]) -> Dict:
+    """Gets features from feast's online store
+
+    Returns:
+        Dict: online featurs from feast 
+    """
     store = FeatureStore(repo_path=Path(config.BASE_DIR, "features"))
     feature_vector = store.get_online_features(
-        feature_refs= feature_refs,
+        feature_refs=feature_refs[0],
         #entity_rows=[{"customer_id": 3}],
         entity_rows=feature_rows,
     ).to_dict()
     return feature_vector
+
+
+def get_feature_entity_df() -> DataFrame:
+    """Gets feature entity from data source
+
+    Returns:
+        DataFrame: feature entity 
+    """
+    data = pd.read_csv(utils.get_data_source_path())
+    customer_tran_id = data['customer_id'].to_list()
+    now = datetime.now()
+    timestamps = [datetime(now.year, now.month, now.day)] * len(customer_tran_id)
+    entity_df = pd.DataFrame.from_dict({"customer_id": customer_tran_id, "event_timestamp": timestamps})
+    # print(entity_df.head())
+    logger.info("feature entity is fetched")
+    return entity_df
---
+++ core/train_and_optimize.py
@@ -0,0 +1,134 @@
+# get the data from the feast feature store
+# split train and test data
+# train and optimize the model
+
+#from core.data import get_feature_entity_df
+from typing import Sequence
+from pandas.core.frame import DataFrame
+import data
+import config
+from config import logger
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
+import joblib
+import optuna
+import numpy as np
+import mlflow
+import os
+
+
+def get_training_data() -> DataFrame:
+    """Gets training data from data source
+
+    Returns:
+        DataFrame: training data
+    """
+    feature_entity = data.get_feature_entity_df()
+
+    # get historic data from feature store
+
+    training_data = data.get_historic_features(feature_entity)
+    logger.info('training data is fetched')
+    # print(training_data.head())
+    return training_data
+
+
+def get_features_and_target() -> Sequence:
+    """splits the data into train and test
+
+    Returns:
+        Pipeline: return train and test data
+    """
+
+    data = get_training_data()
+
+    X = data.drop(['Unnamed: 0', 'customer_id', 'Class', 'created_time'], axis=1, inplace=False)
+
+    y = data[['Class']]
+
+    return X, y
+
+
+def create_model_pipeline() -> Pipeline:
+    """Creates the model pipeline
+
+    Returns:
+        Pipeline: model pipeline
+    """
+    X, y = get_features_and_target()
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+
+    pipeline = Pipeline([
+        ('scalar', StandardScaler()),
+        ('clf', RandomForestClassifier())])
+
+    pipeline.fit(X_train, y_train)
+
+    return pipeline
+
+
+def objective(trial):
+    """Optuna Objective function
+
+    Args:
+        trial ([type]): No of trials on model pramas
+    Raises:
+        optuna.TrialPruned: if trail is pruned
+
+    Returns:
+        [type]: mean cv score
+    """
+    clf__n_estimators = trial.suggest_categorical('clf__n_estimators', [int(x) for x in np.linspace(start=200, stop=2000, num=10)])
+    clf__min_samples_split = trial.suggest_int('clf__min_samples_split', 2, 5, 10)
+    clf__min_samples_leaf = trial.suggest_int('clf__min_samples_leaf', 1, 2, 4)
+    clf__bootstrap = trial.suggest_categorical('clf__bootstrap', [False, True])
+    clf__max_features = trial.suggest_categorical('clf__max_features', ['auto', 'sqrt'])
+    clf__max_depth = trial.suggest_categorical('clf__max_depth', [int(x) for x in np.linspace(10, 110, num=11)].append(None))
+
+    params = {
+        'clf__n_estimators': clf__n_estimators,
+        'clf__min_samples_split': clf__min_samples_split,
+        'clf__min_samples_leaf': clf__min_samples_leaf,
+        'clf__bootstrap': clf__bootstrap,
+        'clf__max_features': clf__max_features,
+        'clf__max_depth': clf__max_depth,
+    }
+
+    if trial.should_prune():
+        raise optuna.TrialPruned()
+
+    pipeline = create_model_pipeline()
+    pipeline.set_params(**params)
+
+    X, y = get_features_and_target()
+
+    return - np.mean(cross_val_score(pipeline, X, y, cv=8, n_jobs=-1))
+
+
+def mlflow_callback(study, trial):
+    """[summary]
+
+    Args:
+        study ([type]): optuna study representing optimisation task
+        trial ([type]): optuna trail object representing process evaluation of objective function 
+    """
+    if study.best_trial.number == trial.number:
+        joblib.dump(trial.user_attrs["best_model"], os.path.join(config.MODEL_DIR, 'model.pkl'))
+
+    with mlflow.start_run(run_name=study.study_name):
+        mlflow.log_params(trial.params)
+        mlflow.log_metrics({"accuracy": trial.value})
+
+
+def optimize_model():
+    pruner = optuna.pruners.MedianPruner(n_startup_trials=5, n_warmup_steps=5)
+    study = optuna.create_study()
+    study.optimize(objective, timeout=3600, pruner=pruner, n_trials=5, callbacks=[mlflow_callback])
+
+
+if __name__ == '__main__':
+    optimize_model()
